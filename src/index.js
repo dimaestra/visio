a0a650182fad1f4920f1928343f5ceb5ac544219
agamdimaestra <73227098+agamdimaestra@users.noreply.github.com>
2021-07-31 17:52:25 +0700

    add router

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let contentDiv = document.getElementById('content');
+
+let routes = {
+  '/': teks,
+  '/index.html': teks,
+};
+
+window.onpopstate = () => {
+  contentDiv.innerHTML = routes[window.location.pathname];
+}
+
+let onNavItemClick = (pathName) => {
+  window.history.pushState({}, pathName, window.location.origin + pathName);
+  contentDiv.innerHTML = routes[pathName];
+}
+
+contentDiv.innerHTML = routes[window.location.pathname];
---
+++ src/teks.js
@@ -0,0 +1,23 @@
+// Oke sip, aku udh masuk
+// mantap
+let teks = `
+const htmlString = /*html*/
+<div class="uk-navbar-container" uk-navbar>
+      <div uk-switcher="connect: #btmbar; animation: uk-animation-fade; toggle: > *">
+        <button class="uk-button uk-button-primary uk-active">
+          <img src="img/btmbar/deteksi.svg" uk-svg>
+        </button>    
+        <button class="uk-button uk-button-secondary">
+          <img src="img/btmbar/baca.svg" uk-svg>
+        </button>
+      </div>
+</div>
+<ul id="btmbar" class="uk-margin uk-switcher">
+    <li>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </li>
+    <li>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </li>
+</ul>
+`;
