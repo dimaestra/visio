8770ff8762b37a974a4e6ff93bcb0f5593fd0aba
agamdimaestra <73227098+agamdimaestra@users.noreply.github.com>
2021-08-27 21:41:33 +0700

    added "cari" page

--- src/cari.js
+++ src/cari.js
@@ -1,20 +1,37 @@
 let cari = `
-<ul id="btmbar" class="uk-switcher uk-flex"style="height: 100vh; align-items: center;">
-    <li>
-      <video 
-        playsinline 
-        autoplay 
-        muted 
-        id="vid"
-      ></video>
-    </li>
-</ul>
+<video
+  playsinline
+  autoplay
+  muted
+   id="vid">
+</video>
 <div class="uk-navbar-container uk-flex-center uk-overlay uk-position-bottom uk-height-small" 
 uk-navbar>
-      <div uk-switcher="connect: #btmbar; animation: uk-animation-fade; toggle: > *">
-        <button class="uk-button uk-button-primary uk-active">
-          <img src="img/btmbar/deteksi.svg" uk-svg>
-        </button>    
-      </div>
+<ul class="uk-tab-bottom" uk-tab>
+  <li class="uk-active">
+    <button class="uk-button uk-button-secondary uk-active">
+    <img src="img/items/elektronik.svg" uk-svg>
+    </button>
+    <a href="">Elektronik</a>
+  </li>
+  <li>
+    <button class="uk-button uk-button-secondary uk-active">
+    <img src="img/items/makanan.svg" uk-svg>
+    </button>
+    <a href="">Makanan</a>
+  </li>
+  <li>
+    <button class="uk-button uk-button-secondary uk-active">
+    <img src="img/items/kendaraan.svg" uk-svg>
+    </button>
+    <a href="">Kendaraan</a>
+  </li>
+  <li>
+    <button class="uk-button uk-button-secondary uk-active">
+    <img src="img/items/orang.svg" uk-svg>
+    </button>
+    <a href="">Orang</a>
+  </li>
+</ul>
 </div>
 `;
