4b70c7639de7d8198b5bae684ce27c21fa6c17a8
Krishna Rowter <krishna.rowter@gmail.com>
2021-08-28 11:33:28 +0700

    Add translate to objectDetection

--- src/deteksi_objek.js
+++ src/deteksi_objek.js
@@ -8,10 +8,24 @@ function modelLoaded() {
   console.log("Model Loaded!");
 }
 
+const translate = text =>
+  fetch("/translate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ originalText: text })
+  });
+
 // Detect objects in the video element
-const doDetect = () => {
+const doDetect = async () => {
   const video = document.getElementById("vid");
-  classifier.classify(video, 1, (error, result) => console.log({ result }));
+  classifier.classify(video, 1, async (error, result) => {
+    const response = await translate(result[0].label);
+    const translated = await response.json().translation;
+
+    // tinggal konekin ke speech synthesis
+  });
   // objectDetector.detect(video, (err, results) => {
   //   console.log(results[0].label);
   //   speech.text = results[0].label;
