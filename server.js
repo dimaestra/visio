f0a7d915bb8ee69970b1cb4e1d5bdcfa9a32e833
agamdimaestra <73227098+agamdimaestra@users.noreply.github.com>
2021-09-04 14:14:53 +0700

    test server

--- server.js
+++ server.js
@@ -1,40 +1,61 @@
-const fs = require("fs");
-const http = require("http");
-const staticFolder = "/";
-const port = process.env.PORT || 8080;
+require("dotenv").config();
 const { translate } = require("bing-translate-api");
-http
-  .createServer(async function (req, res) {
-    const reservedPath = ["/", "/objek"];
-    if (reservedPath.includes(req.url)) {
-      req.url = "index.html";
-    }
-    if (req.method === "POST" && req.url === "/translate") {
-      try {
-        let body = "";
-        await req.on("data", (chunk) => {
-          body += chunk.toString();
-        });
+const vision = require("@google-cloud/vision");
+const multer = require("multer");
+// const reservedPath = ["/", "/objek"];
+const port = process.env.PORT || 8080;
+const path = require("path");
 
-        await translate(JSON.parse(body).originalText, "en", "id", true).then(
-          (result) => {
-            res.writeHead(200);
-            res.end(JSON.stringify(result));
-          }
-        );
-      } catch (err) {
-        res.write(500);
-        res.end(JSON.stringify);
-      }
-    }
-    fs.readFile(__dirname + staticFolder + req.url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
+// express server on port 4000
+const express = require("express");
+const fs = require("fs");
+const { promisify } = require("util");
+
+const unlinkAsync = promisify(fs.unlink);
+
+const upload = multer({
+  dest: "uploads/",
+  filename: function (req, file, cb) {
+    //req.body is empty...
+    //How could I get the new_file_name property sent from client here?
+    cb(null, file.originalname + "-" + Date.now() + ".png");
+  }
+});
+const app = express();
+app.use(express.json());
+
+const client = new vision.ImageAnnotatorClient();
+
+app.post("/upload", upload.single("image"), async (req, res) => {
+  const fileName = req.file.filename;
+  const folder = "uploads";
+
+  const filePath = path.join(__dirname, folder, fileName);
+  // Performs text detection on the local file
+  const [result] = await client.textDetection(filePath);
+  unlinkAsync(filePath);
+  const detections = result.textAnnotations;
+  // detections.forEach(text => console.log(text));
+  res.status(200).send(detections);
+});
+
+app.get(["/", "/objek"], function (req, res, next) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.use(express.static("."));
+
+app.post("/translate", async (req, res) => {
+  try {
+    await translate(req.body.originalText, "en", "id", true).then(result => {
       res.writeHead(200);
-      res.end(data);
+      res.end(JSON.stringify(result));
     });
-  })
-  .listen(port);
+  } catch (err) {
+    console.log({ err });
+    res.status(500).send(err);
+  }
+});
+
+app.post("/ocr", async (req, res) => {});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
